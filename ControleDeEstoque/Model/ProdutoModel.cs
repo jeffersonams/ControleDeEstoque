affba99fcf0f380ed82a9a427fcfe3c9532d175a
Jefferson Santos <jefferson.amsantos@hotmail.com>
2022-12-03 01:07:38 -0300

    Model Produto criado

---
+++ ControleDeEstoque/Model/ProdutoModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ControleDeEstoque.Model
+{
+    public class ProdutoModel
+    {
+        public int Id { get; set; }
+
+        public string Produto { get; set; }
+
+        public string  Preço { get; set; }
+
+        public string Qtde{ get; set; }
+    }
+}
